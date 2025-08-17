96098e75b1a5f721448b53242faa9a5fcbfe5334
victoriarael <victoriarael.atieno@strathmore.edu>
2025-08-17 17:13:02 +0300

    Add database connection file (dbConnect.php)

---
+++ includes/dbConnect.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection file
+
+// Include constants
+require_once "constant.php";
+
+// Create connection
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
